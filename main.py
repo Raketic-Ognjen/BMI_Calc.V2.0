d09883fa66358f9bd18e67f7a6e2e25699331412
Ognjen Raketic <110744188+Raketic-Ognjen@users.noreply.github.com>
2022-09-05 22:34:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,17 @@ print("Welcome to BMI Calculator")
 height = float(input("enter your height in m: \n"))
 weight = float(input("enter your weight in kg: \n"))
 #Calculations
-BMI_index = float(weight) / float(height ** 2)
-BMI_index_float = round(BMI_index, 2)
+i = round(weight / height ** 2)
+BMI_index = i
 
-if BMI_index_float <= 18.5:
-    print(f"Your  BMI is {BMI_index_float}, you are underweight.")
-elif BMI_index_float <= 25:
-    print(f"Your  BMI is {BMI_index_float}, you are normal weight.")
-elif BMI_index_float <= 30:
-    print(f"Your  BMI is {BMI_index_float}, you are you are slightly overweight.")
-elif BMI_index_float <=35:
-    print(f"Your  BMI is {BMI_index_float}, you are obese.")
+
+if BMI_index < 18.5:
+    print(f"Your  BMI is {BMI_index}, you are underweight.")
+elif BMI_index < 25:
+    print(f"Your  BMI is {BMI_index}, you are normal weight.")
+elif BMI_index < 30:
+    print(f"Your  BMI is {BMI_index}, you are you are slightly overweight.")
+elif BMI_index < 35:
+    print(f"Your  BMI is {BMI_index}, you are obese.")
 else:
-    print(f"Your  BMI is {BMI_index_float}, you are clinically obese.")
+    print(f"Your  BMI is {BMI_index}, you are clinically obese")
